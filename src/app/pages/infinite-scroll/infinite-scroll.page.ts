4288bf4f96141eaa5cee49a2bc80b5ad1c2b4fd7
Luis Gordillo C <lgordillo@cecropiasolutions.com>
2020-04-16 15:46:50 -0500

    Infinite scroll implementado.

---
+++ src/app/pages/infinite-scroll/infinite-scroll.page.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
+
+@Component({
+  selector: 'app-infinite-scroll',
+  templateUrl: './infinite-scroll.page.html',
+  styleUrls: ['./infinite-scroll.page.scss'],
+})
+export class InfiniteScrollPage implements OnInit {
+
+  @ViewChild(IonInfiniteScroll, {static: false}) infiniteScroll: IonInfiniteScroll;
+
+  data: any[] = Array(20);
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  loadData(event) {
+    console.log('Cargando siguientes');
+
+    setTimeout(() => {
+      if (this.data.length > 50) {
+        this.infiniteScroll.disabled = true;
+        event.target.complete();
+        return;
+      }
+      const newArr = Array(20);
+      this.data.push(... newArr);
+      event.target.complete();
+    }, 1000);
+  }
+}
